85d7313582cf0fec4574b53857ef2281bf8324c0
SBAUser <sbauser@sbausers-macbook-pro.local>
2019-10-08 13:45:10 -0700

    Initial Commit

---
+++ To-Do List App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  To-Do List App
+//
+//  Created by SBAUser on 10/8/19.
+//  Copyright © 2019 Kyle Moga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
